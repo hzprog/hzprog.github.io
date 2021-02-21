283d496d3a2880540d8fc1446fa427ae6ecdceaf
hzprog <hz.prog@gmail.com>
2021-02-21 14:26:59 +0100

    adding annimation to the mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
-const addChangeToButton = (x) =>{
+// const mainHeaderButton = document.querySelector(".main-header__button");
+const menuMobile = document.querySelector(".menu-mobile");
+
+const addChangeToButton = (x) => {
     x.classList.toggle("change");
+    menuMobile.classList.toggle("menu-mobile__display");
 }
