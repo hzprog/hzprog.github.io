e48169ace79328001ab2ce89ae1d9d45c134f36f
hzprog <hz.prog@gmail.com>
2021-02-22 19:07:49 +0100

    Adding effect and changes to the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 // const mainHeaderButton = document.querySelector(".main-header__button");
 const menuMobile = document.querySelector(".menu-mobile");
+const mobileMenuLinks = document.querySelectorAll(".menu-mobile__links a");
+const mainHeaderButton = document.querySelector(".main-header__button");
 
-const addChangeToButton = (x) => {
-    x.classList.toggle("change");
-    menuMobile.classList.toggle("menu-mobile__display");
-}
+const skill = document.getElementById("skills");
+
+AOS.init();
+
+mainHeaderButton.addEventListener("click", () => {
+  mainHeaderButton.classList.toggle("change");
+  menuMobile.classList.toggle("menu-mobile__display");
+});
+
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.classList.toggle("active");
+    setTimeout(() => {
+      menuMobile.classList.toggle("menu-mobile__display");
+      mainHeaderButton.classList.toggle("change");
+      link.classList.toggle("active");
+    }, 1000);
+  });
+});
