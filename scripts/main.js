35e090c19955e1a8058d1bc1bfeb7cbd043b3d2c
hzprog <hz.prog@gmail.com>
2021-03-23 15:23:20 +0100

    fixing prblms in the header section on mobile vue

--- scripts/main.js
+++ scripts/main.js
@@ -5,32 +5,36 @@ const mainHeaderButton = document.querySelector(".main-header__button");
 
 const skill = document.getElementById("skills");
 
-const form = document.getElementById("contact-form");
+const contactForm = document.getElementById("contact-form");
 
-const handleFormSubmit = (event) => {
+const handleContactFormSubmit = (event) => {
   event.preventDefault();
-  const email = document.getElementById("email");
-  const name = document.getElementById("name");
-  const message = document.getElementById("message");
+  const emailInput = document.getElementById("email");
+  const nameInput = document.getElementById("name");
+  const messageInput = document.getElementById("message");
 
-  if (email.value === "" || name.value === "" || message.value === "") {
+  if (
+    emailInput.value === "" ||
+    nameInput.value === "" ||
+    messageInput.value === ""
+  ) {
   } else {
     Email.send({
       SecureToken: "fbc57371-9cd3-4d22-bb08-e54ae042d88a",
       To: "houariezzine@gmail.com",
       From: "hz.pro666@gmail.com",
-      Subject: `${name.value} messaged you`,
-      Body: `email : ${email.value} \n message : ${message.value}`,
+      Subject: `${nameInput.value} messaged you`,
+      Body: `<h2>Email :</h2> ${emailInput.value} <br/> <h2>Message :</h2> ${messageInput.value}`,
     })
-      .then(function (message) {
-        alert(message);
+      .then((success) => {
+        console.log(success);
       })
-      .catch((message) => {
-        console.log(message);
+      .catch((error) => {
+        console.log(error);
       });
   }
 };
-form.addEventListener("submit", handleFormSubmit);
+contactForm.addEventListener("submit", handleContactFormSubmit);
 
 AOS.init({
   offset: 250,
@@ -54,28 +58,26 @@ mobileMenuLinks.forEach((link) => {
 });
 
 //Get the button:
-mybutton = document.getElementById("scroll-up-button");
+scrollButton = document.getElementById("scroll-up-button");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = () => {
   scrollFunction();
 };
 
-function scrollFunction() {
+const scrollFunction = () => {
   if (
     document.body.scrollTop > 400 ||
     document.documentElement.scrollTop > 400
   ) {
-    mybutton.classList.add("scroll-up-button-visibility");
-    // mybutton.style.display = "block";
+    scrollButton.classList.add("scroll-up-button-visibility");
   } else {
-    // mybutton.style.display = "none";
-    mybutton.classList.remove("scroll-up-button-visibility");
+    scrollButton.classList.remove("scroll-up-button-visibility");
   }
-}
+};
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
